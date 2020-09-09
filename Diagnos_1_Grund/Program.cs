86cd7f9dbd3da417fb64e83ff18ab11f166708fc
Jansan <jansan-26@hotmail.com>
2020-09-09 08:45:36 +0200

    Add project files.

---
+++ Diagnos_1_Grund/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Diagnos_1_Grund
+{
+    class Bil
+    {
+        public string RegistreringsNumber;
+        public string Tillverkare;
+        public int Årtal;
+        public bool Besiktad;
+
+        public Bil(string registreringsNumber, string tillverkare, int årtal, bool besiktad)
+        {
+            RegistreringsNumber = registreringsNumber;
+            Tillverkare = tillverkare;
+            Årtal = årtal;
+            Besiktad = besiktad;
+        }
+
+        public override string ToString()
+        {
+            if (Besiktad)
+            {
+                return "\n\t\t" + Tillverkare + " (" + Årtal + ")"
+                    + "\n\t\t" + RegistreringsNumber
+                    + "\n\t\tBesiktad";
+            }
+            else
+            {
+                return "\n\t\t" + Tillverkare + " (" + Årtal + ")"
+                    + "\n\t\t" + RegistreringsNumber
+                    + "\n\t\tObesiktad";
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
